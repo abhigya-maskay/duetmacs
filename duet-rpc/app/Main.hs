f9f49957f9a8ee338890b3893adf411d19af2393
Abhigya Maskay <ave70011@gmail.com>
2025-09-20 15:17:06 -0500

    Subcommands and flags

--- duet-rpc/app/Main.hs
+++ duet-rpc/app/Main.hs
@@ -1,15 +1,95 @@
+{-# LANGUAGE StrictData #-}
+
 module Main where
 
-import Control.Applicative (some)
-import Options.Applicative (ParserInfo, ReadM, argument, customExecParser, fullDesc, helper, info, metavar, prefs, readerError, showHelpOnEmpty, showHelpOnError, str, (<**>))
+import Data.List (find, intercalate)
+import Data.Maybe (fromMaybe)
+import Options.Applicative ((<**>))
+import qualified Options.Applicative as OA
+
+data LogLevel
+  = LogDebug
+  | LogInfo
+  | LogWarn
+  | LogError
+  deriving (Eq, Show)
+
+data CliCommand
+  = CmdVersion
+  | CmdDoctor
+  | CmdRpc
+  | CmdPrompt
+  deriving (Eq, Show)
+
+data CliOptions = CliOptions
+  { optShowVersion :: Bool
+  , optNoColor :: Bool
+  , optLogLevel :: LogLevel
+  , optCommand :: Maybe CliCommand
+  }
+  deriving (Eq, Show)
 
 main :: IO ()
-main = customExecParser (prefs (showHelpOnEmpty <> showHelpOnError)) opts
+main = OA.customExecParser prefsWithHelp opts >>= runCli
 
-opts :: ParserInfo ()
-opts = info ((() <$ some (argument commandArg (metavar "COMMAND"))) <**> helper) fullDesc
+runCli :: CliOptions -> IO ()
+runCli cliOpts
+  | optShowVersion cliOpts = dispatch CmdVersion
+  | otherwise = mapM_ dispatch (optCommand cliOpts)
+
+prefsWithHelp :: OA.ParserPrefs
+prefsWithHelp = OA.prefs (OA.showHelpOnEmpty <> OA.showHelpOnError)
+
+dispatch :: CliCommand -> IO ()
+dispatch CmdVersion = putStrLn "Version called"
+dispatch CmdDoctor = putStrLn "Doctor called"
+dispatch CmdRpc = putStrLn "RPC called"
+dispatch CmdPrompt = putStrLn "Prompt called"
+
+opts :: OA.ParserInfo CliOptions
+opts = OA.info (cliOptionsParser <**> OA.helper) OA.fullDesc
   where
-    commandArg :: ReadM String
-    commandArg = do
-      cmd <- str
-      readerError ("Invalid command: " ++ cmd)
+    cliOptionsParser = CliOptions <$> versionFlag <*> noColorFlag <*> logLevelOption <*> commandParser
+
+    versionFlag = OA.switch (OA.long "version" <> OA.short 'V' <> OA.help "Print version information")
+    noColorFlag = OA.switch (OA.long "no-color" <> OA.short 'n' <> OA.help "Disable colored output")
+    logLevelOption =
+      OA.option logLevelReader
+        ( OA.long "log-level"
+            <> OA.metavar "LEVEL"
+            <> OA.value defaultLogLevel
+            <> OA.showDefaultWith (const defaultLogLevelName)
+            <> OA.help ("Set log level (" <> intercalate "|" logLevelNames <> ")")
+        )
+    commandParser = OA.optional . OA.hsubparser $ mconcat commandInfos
+
+    commandInfos =
+      [ OA.command "version" (OA.info (pure CmdVersion) (OA.progDesc "Show version information"))
+      , OA.command "doctor" (OA.info (pure CmdDoctor) (OA.progDesc "Run diagnostics"))
+      , OA.command "rpc" (OA.info (pure CmdRpc) (OA.progDesc "Start RPC server"))
+      , OA.command "prompt" (OA.info (pure CmdPrompt) (OA.progDesc "Run prompt workflow"))
+      ]
+
+    logLevelReader :: OA.ReadM LogLevel
+    logLevelReader = OA.eitherReader $ \s ->
+      maybe (Left invalidLevel) Right (lookup s logLevels)
+
+    invalidLevel = "Log level must be one of " <> intercalate ", " logLevelNames
+
+defaultLogLevel :: LogLevel
+defaultLogLevel = LogInfo
+
+defaultLogLevelName :: String
+defaultLogLevelName =
+  fromMaybe "info" (fst <$> find ((== defaultLogLevel) . snd) logLevels)
+
+logLevels :: [(String, LogLevel)]
+logLevels =
+  [ ("debug", LogDebug)
+  , ("info", LogInfo)
+  , ("warn", LogWarn)
+  , ("error", LogError)
+  ]
+
+logLevelNames :: [String]
+logLevelNames = map fst logLevels
