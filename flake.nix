7ff9e7114480fc8be844e9e70c04016f014a51b1
Abhigya Maskay <ave70011@gmail.com>
2025-10-01 17:19:19 -0500

    Upgraded project to new ghc

--- flake.nix
+++ flake.nix
@@ -16,8 +16,8 @@
     forEachSystem = f: lib.genAttrs systems (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        compiler = pkgs.haskell.compiler.ghc9101;
-        hspkgs = pkgs.haskell.packages.ghc9101;
+        compiler = pkgs.haskell.compiler.ghc9122;
+        hspkgs = pkgs.haskell.packages.ghc9122;
       in f pkgs compiler hspkgs);
   in {
     devShells = forEachSystem (pkgs: compiler: hspkgs: {
@@ -26,6 +26,8 @@
           compiler
           hspkgs.cabal-install
           hspkgs.haskell-language-server
+          hspkgs.hlint
+          hspkgs.ormolu
           pkgs.time
         ];
       };
